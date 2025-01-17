3f9bf86670dab9a19571eef36e2be96e0ae8d64a
Lucasdeekay <lucasdeekay98@gmail.com>
2025-01-17 19:44:39 +0100

    Updated settings

--- agbado/settings.py
+++ agbado/settings.py
@@ -99,12 +99,12 @@ DATABASES = {
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('POSTGRES_NAME'),
-#         'USER': config('POSTGRES_USER'),
-#         'PASSWORD': config('POSTGRES_PASSWORD'),
-#         'HOST': config('POSTGRES_HOST', default='localhost'),
-#         'PORT': config('POSTGRES_PORT', default='5432'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST', default='localhost'),
+#         'PORT': config('DB_PORT', default='3306'),
 #     }
 # }
 
