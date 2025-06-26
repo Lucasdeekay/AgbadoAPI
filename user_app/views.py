e06168e63bcc95860e2f147dc17b73ddad3c1d6c
Lucasdeekay <lucasdeekay98@gmail.com>
2025-06-26 22:18:38 +0100

    Updated views

--- user_app/views.py
+++ user_app/views.py
@@ -177,7 +177,7 @@ class UpdateKYCView(APIView):
     def post(self, request):
         user = get_user_from_token(request)
 
-        logger.error(f"Received request data for KYC update: {request.data}")
+        write_to_file(f"Received request data for KYC update: {request.data}")
 
         # 1. Get or create the KYC instance correctly
         # get_or_create returns a tuple: (object, created_boolean)
